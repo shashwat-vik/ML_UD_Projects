8906e8f3dc755aacaf7a3740f894ff3d4ac0c6a6
Shashwat Vikram <shashwat.vikram50@gmail.com>
2017-01-25 00:52:10 +0530

    Pr. 5 (outliers)

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,18 +2,28 @@
 
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+salary, bonus = zip(*data)
+plt.scatter(salary, bonus)
+plt.xlabel('salary')
+plt.ylabel('bonus')
+plt.show()
 
-### your code below
+for key in data_dict.keys():
+    if data_dict[key]['salary'] != 'NaN' and data_dict[key]['bonus'] != 'NaN':
+        if data_dict[key]['salary'] > 10**6 and data_dict[key]['bonus'] >= 5*10**6:
+            print key
 
-
-
+plt.xlabel('salary')
+plt.ylabel('bonus')
+plt.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
-
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    for x, y, p in zip(ages, net_worths, predictions):
+        cleaned_data.append((x, y, abs(y-p)))
+    cleaned_data = sorted(cleaned_data, key=lambda i: i[2])
+    cleaned_data = cleaned_data[:-9]
 
-    
     return cleaned_data
-
